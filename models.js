a8148211bd039d1232c5b9b8e5ffb2eec185a9dc
Soham Gupta <gsoham562@gmail.com>
2025-05-12 22:28:30 +0530

    Almost Completed Guitar Frontend Display

--- models.js
+++ models.js
@@ -14,5 +14,3 @@ let guitar={
   embed: "",
   tags: []
 }
-
-// <iframe width="560" height="202" src="https://www.bandlab.com/embed/?id=a2d52781-ef2d-f011-8b3d-000d3aa44618" allowfullscreen></iframe>
--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,23 @@ let xVal = 0, yVal = 0;
 window.addEventListener("mousemove", (e) => {
   xVal = e.clientX - window.innerWidth / 2;
   yVal = e.clientY - window.innerHeight / 2;
-
+  console.log(parallax_el);
   parallax_el.forEach(el => {
     let speedX = el.dataset.speedx;
     let speedY = el.dataset.speedy;
+    console.log(el);
 
     el.style.transform = `perspective(2300px) translateX(${xVal * speedX}px) translateY(${yVal * speedY}px)`;
   })
 })
+
+function popUpOpen(item){
+  document.getElementById("mainPopup").classList.remove('animate-fade-out');
+  document.getElementById("mainPopup").style.display="block";
+  document.getElementById("mainPopup").classList.add('animate-fade-in');
+}
+
+function closePopup(){
+  document.getElementById("mainPopup").classList.remove('animate-fade-in');
+  document.getElementById("mainPopup").classList.add('animate-fade-out');
+}
